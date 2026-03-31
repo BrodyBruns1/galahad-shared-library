c567dd2ec217bce7c04c9856ba0be424fc1277de
Brody Bruns <brodybruns20@gmail.com>
2026-03-30 19:50:22 -0500

    Fix: OLLAMA_MODEL to qwen3.5:9b (qwen2.5:7b not installed)

--- src/org/galahad/Config.groovy
+++ src/org/galahad/Config.groovy
@@ -4,7 +4,7 @@ class Config implements Serializable {
     // LLM endpoints
     static final String OLLAMA_URL      = 'http://10.201.52.200:11434'
     static final String LMSTUDIO_URL    = 'http://100.71.100.103:1234'
-    static final String OLLAMA_MODEL    = 'qwen2.5:7b'
+    static final String OLLAMA_MODEL    = 'qwen3.5:9b'
 
     // Homelab hosts
     static final String PROXMOX_HOST    = '10.201.52.200'
